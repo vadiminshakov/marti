daba07a4bd3faba0e328843149795af4f0b2c501
Vadim Inshakov <vadiminshakov@gmail.com>
2023-04-19 02:09:01 +0500

    change main

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/vadimInshakov/marti/services"
+	"go.uber.org/zap"
+	"time"
+)
 
+func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	ts := services.NewTradeService()
+	t := time.NewTicker(1 * time.Second)
+	for range t.C {
+		te, err := ts.Trade()
+		if err != nil {
+			logger.Error(err.Error())
+		}
+		if te != nil {
+			logger.Info(te.String())
+		}
+	}
 }
