c4172eacfbac99cb8b22c6839c916198db08ca0c
Vadim Inshakov <vadiminshakov@gmail.com>
2023-04-20 03:01:17 +0500

    refactor detector test

--- services/detector/detector_test.go
+++ services/detector/detector_test.go
@@ -15,7 +15,12 @@ func TestNeedAction(t *testing.T) {
 	buypoint := big.NewFloat(100)
 	window := big.NewFloat(6)
 
-	d := NewDetector(pair, buypoint, window)
+	d := Detector{
+		pair:       pair,
+		buypoint:   buypoint,
+		window:     window,
+		lastAction: entity.ActionBuy,
+	}
 
 	act, err := d.NeedAction(big.NewFloat(100))
 	require.NoError(t, err)
@@ -33,15 +38,34 @@ func TestNeedAction(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, entity.ActionSell, act)
 
+	// после продажи ждем понижения курса
 	act, err = d.NeedAction(big.NewFloat(104))
 	require.NoError(t, err)
-	require.Equal(t, entity.ActionSell, act)
+	require.Equal(t, entity.ActionNull, act)
 
-	act, err = d.NeedAction(big.NewFloat(105))
+	act, err = d.NeedAction(big.NewFloat(101))
 	require.NoError(t, err)
-	require.Equal(t, entity.ActionSell, act)
+	require.Equal(t, entity.ActionNull, act)
 
-	act, err = d.NeedAction(big.NewFloat(106))
+	act, err = d.NeedAction(big.NewFloat(100))
+	require.NoError(t, err)
+	require.Equal(t, entity.ActionNull, act)
+
+	act, err = d.NeedAction(big.NewFloat(98))
+	require.NoError(t, err)
+	require.Equal(t, entity.ActionNull, act)
+
+	// покупаем снова
+	act, err = d.NeedAction(big.NewFloat(97))
+	require.NoError(t, err)
+	require.Equal(t, entity.ActionBuy, act)
+
+	act, err = d.NeedAction(big.NewFloat(102))
+	require.NoError(t, err)
+	require.Equal(t, entity.ActionNull, act)
+
+	// продажа
+	act, err = d.NeedAction(big.NewFloat(103))
 	require.NoError(t, err)
 	require.Equal(t, entity.ActionSell, act)
 
@@ -53,23 +77,25 @@ func TestNeedAction(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, entity.ActionNull, act)
 
+	// покупка
 	act, err = d.NeedAction(big.NewFloat(97))
 	require.NoError(t, err)
 	require.Equal(t, entity.ActionBuy, act)
 
+	// больше не покупаем до продажи
 	act, err = d.NeedAction(big.NewFloat(96))
 	require.NoError(t, err)
-	require.Equal(t, entity.ActionBuy, act)
+	require.Equal(t, entity.ActionNull, act)
 
 	act, err = d.NeedAction(big.NewFloat(95))
 	require.NoError(t, err)
-	require.Equal(t, entity.ActionBuy, act)
+	require.Equal(t, entity.ActionNull, act)
 
 	act, err = d.NeedAction(big.NewFloat(94))
 	require.NoError(t, err)
-	require.Equal(t, entity.ActionBuy, act)
+	require.Equal(t, entity.ActionNull, act)
 
 	act, err = d.NeedAction(big.NewFloat(93))
 	require.NoError(t, err)
-	require.Equal(t, entity.ActionBuy, act)
+	require.Equal(t, entity.ActionNull, act)
 }
