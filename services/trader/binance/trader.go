d784858641dbcf44518bc9219abfecdfe4d2e77b
Vadim Inshakov <vadiminshakov@gmail.com>
2023-04-20 03:03:58 +0500

    add binance trader impl

---
+++ services/trader/binance/trader.go
@@ -0,0 +1,34 @@
+package binance
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/adshao/go-binance/v2"
+	"github.com/vadimInshakov/marti/entity"
+)
+
+type Trader struct {
+	client *binance.Client
+	pair   entity.Pair
+}
+
+func NewTrader(client *binance.Client, pair entity.Pair) (*Trader, error) {
+	return &Trader{pair: pair, client: client}, nil
+}
+
+func (t *Trader) Buy(amount *big.Float) error {
+	_, err := t.client.NewCreateOrderService().Symbol(t.pair.Symbol()).
+		Side(binance.SideTypeBuy).Type(binance.OrderTypeMarket).
+		Quantity(amount.String()).
+		Do(context.Background())
+	return err
+}
+
+func (t *Trader) Sell(amount *big.Float) error {
+	_, err := t.client.NewCreateOrderService().Symbol(t.pair.Symbol()).
+		Side(binance.SideTypeSell).Type(binance.OrderTypeMarket).
+		Quantity(amount.String()).
+		Do(context.Background())
+	return err
+}
