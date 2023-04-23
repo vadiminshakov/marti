9b555e50a65bc5a0279208a94dc4bf56c63ce45e
Vadim Inshakov <vadiminshakov@gmail.com>
2023-04-23 05:21:27 +0500

    small fixes

--- services/tradeservice.go
+++ services/tradeservice.go
@@ -8,9 +8,6 @@ import (
 	"github.com/vadimInshakov/marti/services/wallet"
 )
 
-// TODO make it dynamic
-var amount *big.Float = big.NewFloat(0.02)
-
 type Detector interface {
 	NeedAction(price *big.Float) (entity.Action, error)
 }
@@ -26,14 +23,15 @@ type Trader interface {
 
 type TradeService struct {
 	pair     entity.Pair
+	amount   *big.Float
 	wallet   wallet.Wallet
 	pricer   Pricer
 	detector Detector
 	trader   Trader
 }
 
-func NewTradeService(pair entity.Pair, wallet wallet.Wallet, pricer Pricer, detector Detector, trader Trader) *TradeService {
-	return &TradeService{pair, wallet, pricer, detector, trader}
+func NewTradeService(pair entity.Pair, amount *big.Float, wallet wallet.Wallet, pricer Pricer, detector Detector, trader Trader) *TradeService {
+	return &TradeService{pair, amount, wallet, pricer, detector, trader}
 }
 
 func (t *TradeService) Trade() (*entity.TradeEvent, error) {
@@ -52,22 +50,22 @@ func (t *TradeService) Trade() (*entity.TradeEvent, error) {
 	case entity.ActionBuy:
 		tx := t.wallet.BeginTx()
 
-		if err := t.wallet.Sub(tx, t.pair.To, (&big.Float{}).Mul(amount, price)); err != nil {
+		if err := t.wallet.Sub(tx, t.pair.To, (&big.Float{}).Mul(t.amount, price)); err != nil {
 			if err := tx.Rollback(); err != nil {
 				return nil, errors.Wrap(err, "failed to rollback")
 			}
 
-			return nil, errors.Wrapf(err, "wallet failed to sub %s for %s", amount.Mul(amount, price), t.pair.To)
+			return nil, errors.Wrapf(err, "wallet failed to sub %s for %s", t.amount.Mul(t.amount, price), t.pair.To)
 		}
-		if err := t.wallet.Add(tx, t.pair.From, amount); err != nil {
+		if err := t.wallet.Add(tx, t.pair.From, t.amount); err != nil {
 			if err := tx.Rollback(); err != nil {
 				return nil, errors.Wrap(err, "failed to rollback")
 			}
 
-			return nil, errors.Wrapf(err, "wallet failed to add %s for %s", amount, t.pair.From)
+			return nil, errors.Wrapf(err, "wallet failed to add %s for %s", t.amount, t.pair.From)
 		}
 
-		if err := t.trader.Buy(amount); err != nil {
+		if err := t.trader.Buy(t.amount); err != nil {
 			if err := tx.Rollback(); err != nil {
 				return nil, errors.Wrap(err, "failed to rollback")
 			}
@@ -79,28 +77,28 @@ func (t *TradeService) Trade() (*entity.TradeEvent, error) {
 
 		tradeEvent = &entity.TradeEvent{
 			Action: entity.ActionBuy,
-			Amount: amount,
+			Amount: t.amount,
 			Pair:   t.pair,
 		}
 	case entity.ActionSell:
 		tx := t.wallet.BeginTx()
 
-		if err := t.wallet.Sub(tx, t.pair.From, amount); err != nil {
+		if err := t.wallet.Sub(tx, t.pair.From, t.amount); err != nil {
 			if err := tx.Rollback(); err != nil {
 				return nil, errors.Wrap(err, "failed to rollback")
 			}
 
-			return nil, errors.Wrapf(err, "wallet failed to sub %s for %s", amount, t.pair.From)
+			return nil, errors.Wrapf(err, "wallet failed to sub %s for %s", t.amount, t.pair.From)
 		}
-		if err := t.wallet.Add(tx, t.pair.To, (&big.Float{}).Mul(amount, price)); err != nil {
+		if err := t.wallet.Add(tx, t.pair.To, (&big.Float{}).Mul(t.amount, price)); err != nil {
 			if err := tx.Rollback(); err != nil {
 				return nil, errors.Wrap(err, "failed to rollback")
 			}
 
-			return nil, errors.Wrapf(err, "wallet failed to add %s for %s", amount.Mul(amount, price), t.pair.To)
+			return nil, errors.Wrapf(err, "wallet failed to add %s for %s", t.amount.Mul(t.amount, price), t.pair.To)
 		}
 
-		if err := t.trader.Sell(amount); err != nil {
+		if err := t.trader.Sell(t.amount); err != nil {
 			if err := tx.Rollback(); err != nil {
 				return nil, errors.Wrap(err, "failed to rollback")
 			}
@@ -112,7 +110,7 @@ func (t *TradeService) Trade() (*entity.TradeEvent, error) {
 
 		tradeEvent = &entity.TradeEvent{
 			Action: entity.ActionSell,
-			Amount: amount,
+			Amount: t.amount,
 			Pair:   t.pair,
 		}
 	case entity.ActionNull:
