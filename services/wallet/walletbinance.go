c0897841a580fd66b6e8b0940beabc90840befef
Vadim Inshakov <vadiminshakov@gmail.com>
2023-04-23 05:20:34 +0500

    refactor

--- services/wallet/walletbinance.go
+++ services/wallet/walletbinance.go
@@ -1,30 +1,29 @@
-package binance
+package wallet
 
 import (
 	"errors"
-	"github.com/vadimInshakov/marti/services/wallet"
 	"math/big"
 )
 
 type InMemWallet struct {
-	transactor wallet.Tx
+	transactor Tx
 	balances   map[string]*big.Float
 }
 
-func NewInMemWallet(transactor wallet.Tx, balancesStore map[string]*big.Float) *InMemWallet {
+func NewInMemWallet(transactor Tx, balancesStore map[string]*big.Float) *InMemWallet {
 	return &InMemWallet{transactor: transactor, balances: balancesStore}
 }
 
-func (w *InMemWallet) BeginTx() wallet.Tx {
+func (w *InMemWallet) BeginTx() Tx {
 	return w.transactor
 }
 
-func (w *InMemWallet) Add(tx wallet.Tx, currency string, amount *big.Float) error {
+func (w *InMemWallet) Add(tx Tx, currency string, amount *big.Float) error {
 	w.balances[currency] = amount
 	return nil
 }
 
-func (w *InMemWallet) Sub(tx wallet.Tx, currency string, amount *big.Float) error {
+func (w *InMemWallet) Sub(tx Tx, currency string, amount *big.Float) error {
 	_, ok := w.balances[currency]
 	if ok {
 		w.balances[currency].Sub(w.balances[currency], amount)
