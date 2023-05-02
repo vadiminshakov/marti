a3e33141a0b7e066d8c8413b28715ad17fe9d521
Vadim Inshakov <vadiminshakov@gmail.com>
2023-05-03 01:37:52 +0500

    use decimal values

--- services/detector/detector.go
+++ services/detector/detector.go
@@ -3,6 +3,7 @@ package detector
 import (
 	"context"
 	"fmt"
+	"github.com/shopspring/decimal"
 	"log"
 	"math/big"
 
@@ -16,25 +17,25 @@ type Repository interface {
 
 type Detector struct {
 	pair       entity.Pair
-	buypoint   *big.Float
-	window     *big.Float
+	buypoint   decimal.Decimal
+	window     decimal.Decimal
 	lastAction entity.Action
 }
 
-func NewDetector(client *binance.Client, usebalance float64, pair entity.Pair, buypoint, window *big.Float) (*Detector, error) {
+func NewDetector(client *binance.Client, usebalance decimal.Decimal, pair entity.Pair, buypoint, window decimal.Decimal) (*Detector, error) {
 	res, err := client.NewGetAccountService().Do(context.Background())
 	if err != nil {
 		return nil, err
 	}
 
-	var fromBalance *big.Float
-	var toBalance *big.Float
+	var fromBalance decimal.Decimal
+	var toBalance decimal.Decimal
 	for _, b := range res.Balances {
 		if b.Asset == pair.To {
-			toBalance, _ = new(big.Float).SetString(b.Free)
+			toBalance, _ = decimal.NewFromString(b.Free)
 		}
 		if b.Asset == pair.From {
-			fromBalance, _ = new(big.Float).SetString(b.Free)
+			fromBalance, _ = decimal.NewFromString(b.Free)
 		}
 	}
 
@@ -49,14 +50,14 @@ func NewDetector(client *binance.Client, usebalance float64, pair entity.Pair, b
 		return nil, fmt.Errorf("failed to get price for %s", pair.String())
 	}
 
-	price, _ := new(big.Float).SetString(p[0].Price)
+	price, _ := decimal.NewFromString(p[0].Price)
 
 	// определяем процент доступной для операций второй валюты
-	percent := new(big.Float).Quo(big.NewFloat(usebalance), big.NewFloat(100))
-	toBalance.Mul(toBalance, percent)
+	percent := usebalance.Div(decimal.NewFromInt(100))
+	toBalance = toBalance.Mul(percent)
 
 	// если больше первой валюты, то продаем, если больше второй, то покупаем
-	fromBalanceInSecondCoinsForm := new(big.Float).Mul(fromBalance, price)
+	fromBalanceInSecondCoinsForm := fromBalance.Mul(price)
 	if fromBalanceInSecondCoinsForm.Cmp(toBalance) < 0 {
 		d.lastAction = entity.ActionSell
 	} else {
@@ -68,12 +69,12 @@ func NewDetector(client *binance.Client, usebalance float64, pair entity.Pair, b
 	return d, nil
 }
 
-func (d *Detector) NeedAction(price *big.Float) (entity.Action, error) {
-	nevermindChange := new(big.Float).Quo(d.window, big.NewFloat(2))
+func (d *Detector) NeedAction(price decimal.Decimal) (entity.Action, error) {
+	nevermindChange := d.window.Div(decimal.NewFromInt(2))
 	// check need to sell
 	{
 		if d.lastAction == entity.ActionBuy {
-			sellPoint := new(big.Float).Add(d.buypoint, nevermindChange)
+			sellPoint := d.buypoint.Add(nevermindChange)
 			comparison := price.Cmp(sellPoint)
 			if comparison >= 0 {
 				d.lastAction = entity.ActionSell
@@ -85,7 +86,7 @@ func (d *Detector) NeedAction(price *big.Float) (entity.Action, error) {
 	// check need to buy
 	{
 		if d.lastAction == entity.ActionSell {
-			buyPoint := new(big.Float).Sub(d.buypoint, nevermindChange)
+			buyPoint := d.buypoint.Sub(nevermindChange)
 			comparison := price.Cmp(buyPoint)
 			if comparison <= 0 {
 				d.lastAction = entity.ActionBuy
