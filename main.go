51bb8afda00e4dcae0b7b618915a5b43aff9d87c
Vadim Inshakov <vadiminshakov@gmail.com>
2025-01-04 23:00:27 +0500

    add bybit platform option

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/hirokisan/bybit/v2"
 	"github.com/vadiminshakov/marti/config"
 	"log"
 	"os"
@@ -10,7 +11,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	"github.com/vadiminshakov/marti/services/windowfinder"
+	"github.com/vadiminshakov/marti/services/channel"
 
 	"github.com/adshao/go-binance/v2"
 	"go.uber.org/zap"
@@ -19,6 +20,8 @@ import (
 
 const (
 	restartWaitSec = 30
+
+	platform = "binance"
 )
 
 func main() {
@@ -45,23 +48,44 @@ func main() {
 	g := new(errgroup.Group)
 	var timerStarted atomic.Bool
 	timerStarted.Store(false)
-	for _, c := range configs {
-		conf := c // save value for goroutine
+	for _, conf := range configs {
 		g.Go(func() error {
 			for {
 				ctx, cancel := context.WithTimeout(context.Background(), conf.RebalanceInterval)
 				go timer(ctx, conf.RebalanceInterval, &timerStarted)
 
-				wf := windowfinder.NewBinanceWindowFinder(binanceClient, conf.Minwindow, conf.Pair, conf.StatHours)
-				fn, err := binanceTradeServiceCreator(logger, wf, binanceClient, conf.Pair, conf.Usebalance, conf.PollPriceInterval)
-				if err != nil {
-					logger.Error(fmt.Sprintf("failed to create binance trader service for pair %s, recreate instance after %ds", conf.Pair.String(),
-						restartWaitSec*2), zap.Error(err))
-					time.Sleep(restartWaitSec * 2 * time.Second)
-					continue
+				executor := func(context.Context) error { return nil }
+
+				if platform == "binance" {
+					cf := channel.NewBinanceChannelFinder(binanceClient, conf.Minwindow, conf.Pair, conf.StatHours)
+					executor, err = binanceTradeServiceCreator(logger, cf, binanceClient, conf.Pair, conf.Usebalance, conf.PollPriceInterval)
+					if err != nil {
+						logger.Error(fmt.Sprintf("failed to create binance trader service for pair %s, recreate instance after %ds", conf.Pair.String(),
+							restartWaitSec*2), zap.Error(err))
+						time.Sleep(restartWaitSec * 2 * time.Second)
+						continue
+					}
 				}
 
-				if err := fn(ctx); err != nil {
+				if platform == "bybit" {
+					bybitClient := bybit.NewClient().WithAuth(apikey, secretKey)
+
+					cf := channel.NewBybitChannelFinder(bybitClient, conf.Minwindow, conf.Pair, conf.StatHours)
+
+					executor = func(context.Context) error {
+						buyprice, channel, err := cf.GetTradingChannel()
+						if err != nil {
+							return errors.Wrapf(err, "failed to find window for %s", conf.Pair.String())
+						}
+
+						fmt.Printf("buyprice: %v, channel: %v\n", buyprice, channel)
+						select {}
+
+						return nil
+					}
+				}
+
+				if err = executor(ctx); err != nil {
 					cancel()
 					if errors.Is(err, context.DeadlineExceeded) {
 						logger.Info("recreate instance", zap.String("pair", conf.Pair.String()))
