b2134f064bd491ba49840061c85b2d4688b6c19b
Vadim Inshakov <vadiminshakov@gmail.com>
2025-05-14 11:55:34 +0500

    refactor

---
+++ internal/app/services/trader/binance.go
@@ -0,0 +1,38 @@
+package trader
+
+import (
+	"context"
+
+	"github.com/adshao/go-binance/v2"
+	"github.com/shopspring/decimal"
+	"github.com/vadiminshakov/marti/internal/app/entity"
+)
+
+type BinanceTrader struct {
+	client *binance.Client
+	pair   entity.Pair
+}
+
+func NewBinanceTrader(client *binance.Client, pair entity.Pair) (*BinanceTrader, error) {
+	return &BinanceTrader{pair: pair, client: client}, nil
+}
+
+func (t *BinanceTrader) Buy(amount decimal.Decimal) error {
+	amount = amount.RoundFloor(4)
+	_, err := t.client.NewCreateOrderService().Symbol(t.pair.Symbol()).
+		Side(binance.SideTypeBuy).Type(binance.OrderTypeMarket).
+		Quantity(amount.String()).
+		Do(context.Background())
+
+	return err
+}
+
+func (t *BinanceTrader) Sell(amount decimal.Decimal) error {
+	amount = amount.RoundFloor(4)
+	_, err := t.client.NewCreateOrderService().Symbol(t.pair.Symbol()).
+		Side(binance.SideTypeSell).Type(binance.OrderTypeMarket).
+		Quantity(amount.String()).
+		Do(context.Background())
+
+	return err
+}
---
+++ internal/app/services/trader/bybit.go
@@ -0,0 +1,49 @@
+package trader
+
+import (
+	"github.com/hirokisan/bybit/v2"
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
+	"github.com/vadiminshakov/marti/internal/app/entity"
+)
+
+type BybitTrader struct {
+	client *bybit.Client
+	pair   entity.Pair
+}
+
+func NewBybitTrader(client *bybit.Client, pair entity.Pair) (*BybitTrader, error) {
+	return &BybitTrader{pair: pair, client: client}, nil
+}
+
+func (t *BybitTrader) Buy(amount decimal.Decimal) error {
+	amount = amount.RoundFloor(4)
+	_, err := t.client.V5().Order().CreateOrder(bybit.V5CreateOrderParam{
+		Category:   "spot",
+		Symbol:     bybit.SymbolV5(t.pair.Symbol()),
+		Side:       bybit.SideBuy,
+		OrderType:  bybit.OrderTypeMarket,
+		Qty:        amount.String(),
+		IsLeverage: nil,
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to create buy order")
+	}
+	return nil
+}
+
+func (t *BybitTrader) Sell(amount decimal.Decimal) error {
+	amount = amount.RoundFloor(4)
+	_, err := t.client.V5().Order().CreateOrder(bybit.V5CreateOrderParam{
+		Category:   "spot",
+		Symbol:     bybit.SymbolV5(t.pair.Symbol()),
+		Side:       bybit.SideSell,
+		OrderType:  bybit.OrderTypeMarket,
+		Qty:        amount.String(),
+		IsLeverage: nil,
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to create sell order")
+	}
+	return nil
+}
