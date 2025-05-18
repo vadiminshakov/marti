14b6f8f56920eb93d29358dca4c08187acae78a3
Vadim Inshakov <vadiminshakov@gmail.com>
2025-05-18 22:36:10 +0500

    refactor main

--- cmd/main.go
+++ cmd/main.go
@@ -3,23 +3,56 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/vadiminshakov/marti/config"
 	"github.com/vadiminshakov/marti/internal/app"
+	"github.com/vadiminshakov/marti/internal/app/clients"
 	"go.uber.org/zap"
 )
 
 func main() {
-	bot, err := app.NewTradingBot(config.Config{}, nil)
+	configs, err := config.Get()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
+	for _, config := range configs {
+		var client interface{}
+		switch config.Platform {
+		case "binance":
+			apiKey := os.Getenv("BINANCE_API_KEY")
+			apiSecret := os.Getenv("BINANCE_API_SECRET")
+			if apiKey == "" || apiSecret == "" {
+				log.Fatal("BINANCE_API_KEY and BINANCE_API_SECRET environment variables must be set")
+			}
+			client = clients.NewBinanceClient(apiKey, apiSecret)
+		case "bybit":
+			apiKey := os.Getenv("BYBIT_API_KEY")
+			apiSecret := os.Getenv("BYBIT_API_SECRET")
+			if apiKey == "" || apiSecret == "" {
+				log.Fatal("BYBIT_API_KEY and BYBIT_API_SECRET environment variables must be set")
+			}
+			client = clients.NewBybitClient(apiKey, apiSecret)
+		default:
+			log.Fatal("unsupported platform")
+		}
 
-	// Run the trading bot
-	if err := bot.Run(context.Background(), logger); err != nil {
-		log.Fatal(err)
+		bot, err := app.NewTradingBot(config, client)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		logger, _ := zap.NewProduction()
+		defer logger.Sync()
+
+		// Run the trading bot
+		go func() {
+			if err := bot.Run(context.Background(), logger); err != nil {
+				log.Fatal(err)
+			}
+		}()
 	}
+
+	select {}
 }
